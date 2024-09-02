a11e85bed250d890e0025465282d01d2f17d19ab
Leonardo <leonardo.alonso@benandfrank.com>
2024-09-01 21:59:56 -0600

    feat(database): initialize mysql database connection
    This change has a new file to manage the database connection
    also adds a config file to read from the environment varibles the
    values to connect to de database, this config is also a global variable
    calles Envs

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
+
 	"github.com/leonardoAlonso/go-ecom/cmd/api"
+	"github.com/leonardoAlonso/go-ecom/config"
+	"github.com/leonardoAlonso/go-ecom/db"
 )
 
 func main() {
-	server := api.NewApiServer(":8080", nil) // Pointer to the struct
-	if err := server.Run(); err != nil {     // Run on the same memory address
+	db, err := db.NewMySqlStorage(
+		mysql.Config{
+			User:                 config.Envs.DBUser,
+			Passwd:               config.Envs.DBPassword,
+			Addr:                 config.Envs.DBAddress,
+			DBName:               config.Envs.DBName,
+			Net:                  "tcp",
+			AllowNativePasswords: true,
+			ParseTime:            true,
+		},
+	)
+	if err != nil {
 		log.Fatal(err)
 	}
+
+	InitStorage(db) // Pointer to the database connection
+
+	server := api.NewApiServer(":8080", db) // Pointer to the struct
+	if err := server.Run(); err != nil {    // Run on the same memory address
+		log.Fatal(err)
+	}
+}
+
+func InitStorage(db *sql.DB) {
+	err := db.Ping() // check if the connection is alive
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// start the connection
+	log.Println("Connected to the database")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/leonardoAlonso/go-ecom
 
 go 1.22.6
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
